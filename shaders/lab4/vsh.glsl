b8960b399f4d7eeb2d88afd12fc00de64cd9d765
Alexander Permyakov <pamugk@gmail.com>
2021-08-06 07:49:05 +0500

    Fixes
    - fixed lab4 shaders,
    - fixed lab4 config

--- shaders/lab4/vsh.glsl
+++ shaders/lab4/vsh.glsl
@@ -1,9 +1,9 @@
 #version 450
 
 layout(location = 0) in vec3 a_position;
-layout(location = 1) in vec3 a_normal;
-layout(location = 2) in vec2 a_texCoord;
-layout(location = 3) in int a_texLayer;
+layout(location = 1) in vec2 a_texCoord;
+layout(location = 2) in int a_texLayer;
+layout(location = 3) in vec3 a_normal;
 
 uniform mat4 u_mvp; // MVP-матрица
 uniform mat3 u_n; // Матрица нормалей
@@ -16,7 +16,7 @@ out vec3 v_pos;
 void main()
 {
     v_texCoord = a_texCoord;
-    v_texLayer = a_texLayer
+    v_texLayer = a_texLayer;
     v_normal = normalize(u_n * a_normal);
     v_pos = a_position;
     gl_Position = u_mvp * vec4(a_position, 1.0);
